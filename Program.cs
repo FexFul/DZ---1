6085884ba603943f8bc7dce6c0d5c3fa69e9af39
Niyaz <fexful@gmail.com>
2022-09-05 00:36:32 +0500

    First commit
    База программы

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ___1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fullName = "Степанович Степан Степанов";
+            UInt32 age = 22;
+            String mail = @"StepanSuper@gmail.com";
+            UInt32 scoresProgramming = 76;
+            UInt32 scoresMathematics = 80;
+            UInt32 scoresPhysics = 60;
+
+            Console.WriteLine("")
+        }
+    }
+}
