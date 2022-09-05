6ec8de67a281af19cf3a1470f3d459d61e54ed7d
Niyaz <fexful@gmail.com>
2022-09-05 21:33:17 +0500

    Update

--- Program.cs
+++ Program.cs
@@ -13,20 +13,20 @@ namespace DZ___1
             String fullName = "Степанович Степан Степанов";
             ushort age = 22;
             String mail = @"StepanSuper@gmail.com";
-            ushort scoresProgramming = 76;
-            ushort scoresMathematics = 80;
-            ushort scoresPhysics = 60;
-
-            UInt32 allScores = 0;
-            UInt32 averageScore = 0;
+            double scoresProgramming = 76.2;
+            double scoresMathematics = 80.12;
+            double scoresPhysics = 60.3;
 
             Console.WriteLine($"Ф.И.О: {fullName};\n Возраст: {age};\n Почта: {mail};\n Баллы по программированию: {scoresProgramming};\n" +
                 $" Баллы по математике: {scoresMathematics};\n Баллы по физике: {scoresPhysics}");
 
             Console.ReadLine();
 
-            allScores = (UInt32)(scoresProgramming + scoresMathematics + scoresPhysics);
-            averageScore = (UInt32)((scoresProgramming + scoresMathematics + scoresPhysics) / 3);
+            double allScores = 0;
+            double averageScore = 0;
+
+            allScores = (scoresProgramming + scoresMathematics + scoresPhysics);
+            averageScore = (scoresProgramming + scoresMathematics + scoresPhysics) / 3;
 
             Console.WriteLine($"Общий балл: {allScores};\n Средний балл: {averageScore}");
 
